da83ca56a786fce677ae77e23ed3361b3201a71a
zhangpeng <zhpeng@aliyun.com>
2021-01-29 22:53:02 +0800

    merge

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{title}}</p>
+    <button @click="save">点击</button>
   </div>
 </template>
+<script>
+import log from 'z-daydao-ui';
+export default {
+  data() {
+    return {
+      title:'about'
+    }
+  },
+
+  created(){
+    console.log(111);
+    log.log(2323)
+
+  },
+
+  mounted(){
+    this.warn('2233')
+  },
+  
+  methods:{
+    save(){
+      this.util.warn('23232')
+    }
+  }
+
+
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,7 +7,6 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
 @Component({
   components: {
     HelloWorld,

21e2fe25215fa6b744db6971379ae502ca2f240e
zhangpeng <peng_zhang@daydao.com>
2021-06-02 17:46:34 +0800

    merge

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+module.exports = {
+    devServer: {
+        host: 'dev.daydao.com',
+        port: 9600,
+        proxy: {
+            '/api/*': {
+                target: 'http://127.0.0.1:8081/',
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+    }
+}
